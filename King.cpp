3cfb726ba19733ece26c04459dd99e83f2741f9d
Usman-0515 <f250515@cfd.nu.edu.pk>
2026-05-08 19:00:10 +0500

    update: improving moves
    -stop move at same place

--- King.cpp
+++ King.cpp
@@ -1,7 +1,7 @@
 #include "King.h"
-
+#include "Board.h"
 King::King(char s) : Piece(s) {}
-
 bool King::isValidMove(int sx, int sy, int dx, int dy, Board* board) {
-	return abs(dx - sx) <= 1 && abs(dy - sy) <= 1;
+    if (sx == dx && sy == dy) return false;  
+    return abs(dx - sx) <= 1 && abs(dy - sy) <= 1;
 }
